ee583a50d0a58cc0f0e3a7b3c6441c2bcdaffcfb
imadnajam <118115991+Imadnajam@users.noreply.github.com>
2024-01-21 19:06:20 +0100

    add redux to the rect project

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,6 @@ const Navbar = () => {
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item active">
                         <Link to={'/login'} className='nav-link'>Login</Link>
-
                     </li>
                     <li class="nav-item active">
                         <Link to={'/sign_up'} className='nav-link'>sign up</Link>
